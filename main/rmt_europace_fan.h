6e37c80cc067415ee2570293b939d8b5357014e4
Vergil Cola <vpcola@gmail.com>
2017-09-18 22:36:41 +0800

    Added rmt control for europace electric fan

---
+++ main/rmt_europace_fan.h
@@ -0,0 +1,18 @@
+#ifndef _RMT_EUROPACE_FAN_H_
+#define _RMT_EUROPACE_FAN_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "rmt_utils.h"
+
+void toggleOnOff(void);
+void toggleSpeed(void);
+void toggleOscillate(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
